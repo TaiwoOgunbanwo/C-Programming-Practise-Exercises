fd4cc15031e8c7f87f41954ff9c5166bd3e96477
Taiwo Ogunbanwo <148710861+TaiwoOgunbanwo@users.noreply.github.com>
2026-05-27 18:49:55 +0100

    Delete C2P5.c

--- C2P5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int x;
-    printf("Enter your value for x: \n");
-    scanf("%d", &x);
-    printf("%d\n", (3 * x * x * x * x * x) + (2 * x * x * x * x) - (5 * x * x * x) - (x * x) + (7 * x) - 6);
-    return 0;
-}
